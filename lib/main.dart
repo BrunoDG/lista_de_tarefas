1d8f8d60c26d3802030cb0b1942c4e01718aa201
Bruno Dias <brunodg.dev@gmail.com>
2021-02-16 04:05:24 -0300

    To-Do List App finished.

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,21 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      _toDoList.sort((a, b) {
+        if (a["ok"] && !b["ok"])
+          return 1;
+        else if (!a["ok"] && b["ok"])
+          return -1;
+        else
+          return 0;
+      });something
+    });
+  }
+
   @override
   void initState() {
     super.initState();
@@ -79,10 +94,13 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.only(top: 10.0),
-              itemCount: _toDoList.length,
-              itemBuilder: buildItem,
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: ListView.builder(
+                padding: EdgeInsets.only(top: 10.0),
+                itemCount: _toDoList.length,
+                itemBuilder: buildItem,
+              ),
             ),
           ),
         ],
@@ -134,6 +152,7 @@ class _HomeState extends State<Home> {
             duration: Duration(seconds: 2),
           );
 
+          ScaffoldMessenger.of(context).removeCurrentSnackBar();
           ScaffoldMessenger.of(context).showSnackBar(snack);
         });
       },
